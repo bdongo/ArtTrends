738d57120ec2600ccfe99d6f9670a35e08da5fc0
brandon <brandon.w.leung@gmail.com>
2023-04-04 16:55:52 -0700

    canvas and api links

--- src/index.js
+++ src/index.js
@@ -1,7 +1,131 @@
 import * as TEST from './scripts/testFetch.js';
 import Photo from './scripts/photo.js';
+import * as d3 from 'd3';
+import { async } from 'regenerator-runtime';
+
+const photoArr = [];
+// TEST.cleveland()
+
+// TEST.harvard() 
+
+// TEST.chicago() // slow ?..
+const display = document.querySelector('#img-container')
+
+const imgNum = 24;
+
+let width = window.innerWidth * 0.67;
+
+console.log(window.innerWidth);
+
+// console.log(width)
+// console.log(display, "get attribute")
+const height = 350;
+
+const createCanvas = d3.select('div#img-container')
+    .append('canvas')
+    .attr('width', width)
+    .attr('height', 350)
+    .attr('id', 'canvas');
 
 
-TEST.cleveland()
 
-TEST.harvard()
+
+addEventListener("resize", (e) => {
+    console.log(window.innerWidth, "assigning innerwidth");
+    width = window.innerWidth * 0.67;
+    createCanvas
+        .attr('width', width);
+
+    svg
+        .attr('width', width);
+
+    svg.selectAll('rect')
+        .attr('x', d => d * (width / imgNum))
+        .attr('width', width / imgNum - 4);
+
+    createCanvas
+        .selectAll("img")
+        .attr('x', d => d * (width / imgNum))
+        .attr('width', width / imgNum - 4);
+
+    render();
+
+})
+
+
+
+const render = () => {
+    const ctx = createCanvas.node().getContext('2d')
+
+
+    for (let i = 0; i < imgNum; i++){
+        let imgTest = new Image()
+        imgTest.onload = () => {
+            ctx.drawImage(imgTest, 
+                300 + i * 10, 0, width / imgNum - 3, height,
+                i * (width / imgNum), 0, width / imgNum - 4, height)
+        }
+        imgTest.src = "https://openaccess-cdn.clevelandart.org/1922.1133/1922.1133_web.jpg"
+        // console.log(imgTest)
+    }
+}
+
+render()
+
+
+const svg = d3.select('div#img-container')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+svg
+    .on('mouseover', (e) => {
+        console.log(e)
+        e.target.classed("hidden", false);
+    })
+    .on('mouseout', (e) => {
+        e.classed("hidden", true)
+    });
+
+// svg
+//     .selectAll('rect')
+//     .data(d3.range(imgNum))
+//     .join('rect')
+//     .attr('x', d => d * (width / imgNum))
+//     .attr('width', width / imgNum - 4)
+//     .attr('height', height);
+
+svg
+    .selectAll("image")
+    .data(d3.range(imgNum))
+    .join('image')
+    .attr('href', "https://openaccess-cdn.clevelandart.org/1922.1133/1922.1133_web.jpg")
+    .attr('x', d => d * (width / imgNum))
+    .attr('width', width / imgNum - 4)
+    .attr("class", "hidden")
+    .attr('height', height);
+
+// const mask = svg.append('mask')
+//     .attr("id", "clipping-mask");
+
+// mask.append('rect')
+//     .attr("height", height)
+//     .attr("width", 30)
+//     .attr("x", "25%")
+//     .attr("fill", "white");
+
+// const image = svg.append('image') 
+//     .attr('href', "https://openaccess-cdn.clevelandart.org/1922.1133/1922.1133_web.jpg")
+//     .attr('height', height)
+//     .attr('mask', "url(#clipping-mask");
+
+// const image2 = svg.append('image')
+//     .attr('href', "https://openaccess-cdn.clevelandart.org/1928.8/1928.8_web.jpg")
+//     .attr('height', height)
+//     .attr('mask', "url(#clipping-mask");
+
+
+
+
+
+
--- src/scripts/testFetch.js
+++ src/scripts/testFetch.js
@@ -1,5 +1,7 @@
 
-const harvardUrl = "https://api.harvardartmuseums.org/object?q=totalpageviews:0&size=10&apikey=12403398-3c09-42ff-af07-f434bfd000a1"
+const harvardUrl = "https://api.harvardartmuseums.org/object?size=10&apikey=12403398-3c09-42ff-af07-f434bfd000a1"
+
+// https://api.harvardartmuseums.org/object?apikey=YOUR_API_KEY&hasimage=true&permissionlevel=0
 
 export const harvard = async (e) => {
 
@@ -7,9 +9,9 @@ export const harvard = async (e) => {
         headers: { "Accept": "application/json" }
     })
 
-    console.log(res, "response")
+    // console.log(res, "response")
     let data = await res.json()
-    console.log(data, "data")
+    // console.log(data, "data")
     // console.log(data.records[0].primaryimageurl)
     // console.log(data.response[0])
     for (let i = 0; i < data.records.length; i++) {
@@ -17,7 +19,7 @@ export const harvard = async (e) => {
         if (data.records[i].primaryimageurl) {
             let newImg = document.createElement("img")
             newImg.src = data.records[i].primaryimageurl
-            document.querySelector("#display-element").appendChild(newImg)
+            document.querySelector("#img-container").appendChild(newImg)
         }
     }
 
@@ -45,7 +47,7 @@ export const cleveland = async (e) => {
         if (data.data[i].images.web.url) {
             let newImg = document.createElement("img")
             newImg.src = data.data[i].images.web.url
-            document.querySelector("#display-element").appendChild(newImg)
+            document.querySelector("#img-container").appendChild(newImg)
         }
     }
 
@@ -63,10 +65,12 @@ export const chicago2 = async (url) => {
         headers: { "Accept": "application/json" }
     })
 
-    // console.log(res, "response")
+    // console.log(res, "chicago2 response")
     let data = await res.json()
-    // console.log(data, "data")
-    return (data.config.iiif_url + "/" + data.data.image_id + "/full/843,/0/default.jpg")
+    // console.log(data, "chicago2 data")
+    let linkStr = `${data.config.iiif_url}/${data.data.image_id}/full/843,/0/default.jpg`
+    // console.log(linkStr, "link")
+    return linkStr
 }
 
 // ^^to get each img link
@@ -77,19 +81,20 @@ export const chicago = async (e) => {
         headers: { "Accept": "application/json" }
     })
 
-    console.log(res, "response")
+    // console.log(res, "response")
     let data = await res.json()
-    console.log(data, "data")
+    // console.log(data, "data")
   
     // console.log(link)
     for (let i = 0; i < data.data.length; i++) {
 
-        if (data.data[i].images.web.url) {
-            const link = await data.data[i].api_link.json()
+     
+            const link = await data.data[i].api_link
             let newImg = document.createElement("img")
-            newImg.src = chicago2(link)
-            document.querySelector("#display-element").appendChild(newImg)
-        }
+            let imgURL = await chicago2(link)
+            newImg.src = imgURL
+        document.querySelector("#img-container").appendChild(newImg)
+        
     }
 }
 
